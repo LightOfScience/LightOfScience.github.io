e109da5801c8a362294dfd0968b124afd3428ceb
Dhiman Sarkar <dhiman.eminem@gmail.com>
2019-03-08 04:01:24 +0530

    Developping Beta Started | v3.0.7

---
+++ assets/js/custom_jscript.js
@@ -0,0 +1,76 @@
+/* Hamber Menu Toggle Action -START */
+let NavStatus = false;
+
+let toggleNav = function()
+{
+    let NavMenu = document.querySelector(".navmenu");
+
+    if (NavStatus === false)
+    {
+        NavMenu.style.visibility = "visible";
+        NavMenu.style.height = "auto";
+        NavMenu.style.opacity = "1";
+        NavStatus = true;
+    }
+    else if (NavStatus === true)
+    {
+        NavMenu.style.visibility = "";
+        NavMenu.style.height = "0";
+        NavMenu.style.opacity = "";
+        NavStatus = false;
+    }
+}
+/* Hamber Menu Toggle Action -END */
+let Visibility = false;
+
+let toggleVisibility = function()
+{
+    let element = document.querySelector(".toggle-visibility");
+    let elementToggler = document.querySelector(".visibility-toggler");
+
+    if (Visibility === false)
+    {
+        element.style.visibility = "visible";
+        element.style.opacity = "1";
+        element.classList.add('slideInRight');
+        elementToggler.innerHTML = 'Hide Description';
+        Visibility = true;
+        
+    }
+    else if (Visibility === true)
+    {
+        element.classList.remove('slideInRight');
+        element.style.opacity = "0";
+        element.style.visibility = "hidden";
+        elementToggler.innerHTML = 'Show Description';
+        Visibility = false;
+    }
+}
+/* Toggle Visibility */
+
+
+
+
+/* Scrool to Top Javascript */
+$(function(){
+ 
+$(document).on( 'scroll', function(){
+
+if ($(window).scrollTop() > 100) {
+$('.scroll-top-wrapper').addClass('show');
+} else {
+$('.scroll-top-wrapper').removeClass('show');
+}
+});
+ 
+$('.scroll-top-wrapper').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+element = $('body');
+offset = element.offset();
+offsetTop = offset.top;
+$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
+/* Scrool to Top Javascript */
