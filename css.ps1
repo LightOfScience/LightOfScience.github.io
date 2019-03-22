b12c2e5487babb02ffa8faf64ba2aee6b2515cc2
Dhiman Sarkar <dhiman.eminem@gmail.com>
2019-03-22 17:14:29 +0530

    about page finished

--- css.ps1
+++ css.ps1
@@ -1,2 +1,2 @@
-﻿sass --watch .\assets\css\global_style.scss .\assets\css\global_style.css --style compressed;
+﻿sass --watch .\assets\css\global_style.scss .\assets\css\global_style.css --style compressed --update -c --trace;
 clear;
