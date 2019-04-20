1f7e0658d31b93d31d835ac67686ae120358b882
Dhiman Sarkar <dhiman.eminem@gmail.com>
2019-04-21 00:08:37 +0530

    admin upload added

--- push.ps1
+++ push.ps1
@@ -12,3 +12,14 @@ for ($i = 3; $i -ge 0; $i-- )
     Start-Sleep -s 1;
 }
 git push DevBeta +DevBeta:master --tag;
+
+
+cd '.\admin.LightOfScience.tk'
+for ($i = 3; $i -ge 0; $i-- )
+{
+    Write-Progress -Activity "Pushing to Admin Website | Waiting:" -Status "$i sec";
+    Start-Sleep -s 1;
+}
+git push --all admin;
+git push admin --tags;
+cd..
