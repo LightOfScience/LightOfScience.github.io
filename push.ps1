f6839b188419959bf7ac8946254fa70623a881b2
Dhiman Sarkar <dhiman.eminem@gmail.com>
2019-05-18 00:07:29 +0530

    auto upload file modified

--- push.ps1
+++ push.ps1
@@ -1,11 +1,3 @@
-for ($i = 3; $i -ge 0; $i-- )
-{
-    Write-Progress -Activity "Pushing to Website | Waiting:" -Status "$i sec";
-    Start-Sleep -s 1;
-}
-git push --all website;
-git push website --tags;
-
 cd '.\admin.LightOfScience.tk'
 for ($i = 3; $i -ge 0; $i-- )
 {
@@ -15,3 +7,22 @@ for ($i = 3; $i -ge 0; $i-- )
 git push --all admin;
 git push admin --tags;
 cd..
+
+cd '.\dev.LightOfScience.tk'
+for ($i = 3; $i -ge 0; $i-- )
+{
+    Write-Progress -Activity "Pushing to Dev Website | Waiting:" -Status "$i sec";
+    Start-Sleep -s 1;
+}
+git push --all dev;
+git push dev --tags;
+cd..
+
+for ($i = 3; $i -ge 0; $i-- )
+{
+    Write-Progress -Activity "Pushing to Website | Waiting:" -Status "$i sec";
+    Start-Sleep -s 1;
+}
+git push --all website;
+git push website --tags;
+
