00540a2f40359ac6a7c3c62c2850c4f95b6067fd
Dhiman Sarkar <dhiman.eminem@gmail.com>
2019-11-05 17:50:44 +0530

    book & publications update started

--- build.ps1
+++ build.ps1
@@ -3,5 +3,5 @@ start powershell {
 }
 start powershell {
     cd .\assets\css\
-    sass --watch --style=compressed globalstyle.scss:globalstyle.css home.scss:home.css
+    sass --watch --style=compressed globalstyle.scss:globalstyle.css home.scss:home.css publications_books.scss:publications_books.css
 }
