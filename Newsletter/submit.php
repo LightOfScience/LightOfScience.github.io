1cc70aeb14393bc6395354796e903ed7cadc2561
Dhiman Sarkar <dhiman-eminem@users.noreply.github.com>
2019-11-30 01:22:19 +0530

    Submit to Database Script

---
+++ Newsletter/submit.php
@@ -0,0 +1,10 @@
+<?php
+    include_once 'db_conn.php';
+
+    $name = $_GET['name'];
+    $email = $_GET['email'];
+    $sql = "INSERT INTO `newsletter_subscriber_list` (`Name`, `Email`) VALUES ('$name','$email');";
+    mysqli_query($conn, $sql);
+
+    header("Location: https://LightOfScience.tk?NewsletterSubmit=SUCCESSFUL");
+?>
