018bf39277943e83ab6f183fd727e4840c625484
Dhiman Sarkar <dhiman.eminem@gmail.com>
2020-04-23 15:38:10 +0530

    news, blog added

--- assets/js/front-page/blog.js
+++ assets/js/front-page/blog.js
@@ -7,13 +7,15 @@ blog_btn_left.onclick = () => {blog_slide.scrollBy({left:-scrlX, behavior:"smoot
 blog_btn_right.onclick = () => {blog_slide.scrollBy({left:scrlX, behavior:"smooth" });}
 
 var blog_tmpVar = blog_scrlX;
+var y =0;
 setInterval(() => {
     if(blog_slide.scrollLeft == 0){
         blog_tmpVar = blog_scrlX;
+        y=0;
     }
-    else if(blog_slide.scrollLeft == blog_slide.scrollLeftMax){
+    else if(y >= blog_slide.scrollWidth-blog_scrlX-2){
         blog_tmpVar = -blog_scrlX;
     }
     blog_slide.scrollBy({left:blog_tmpVar, behavior:"smooth" });
-
+    y = y + blog_scrlX;
 }, 3511); 
