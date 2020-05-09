93a93945c8787bf3a3cd4d6ba9ea7b7f3d302630
Dhiman Sarkar <dhiman.eminem@gmail.com>
2020-05-09 10:39:29 +0530

    Flickr Component Added

--- netlify-custom-widgets-components.js
+++ netlify-custom-widgets-components.js
@@ -20,3 +20,27 @@ CMS.registerEditorComponent({
     toPreview: function(obj) {return ('<img src="https://www.gstatic.com/youtube/img/branding/youtubelogo/svg/youtubelogo.svg" alt="Youtube Video"/>');}
 });
 
+// Flickr
+CMS.registerEditorComponent({
+// Internal id of the component
+    id: "flickr",
+    // Visible label
+    label: "Flickr Album",
+    // Fields the user need to fill out when adding an instance of the component
+    fields: 
+        [
+            {name: 'album_title', label: 'Album Title', widget: 'string'},
+            {name: 'album_link', label: 'Album Link', widget: 'string'},
+            {name: 'album_image', label: 'First Image Link', widget: 'string'}
+        ],
+    // Pattern to identify a block as being an instance of this component
+    pattern: /^\{\%include\sflickr-embed.html\salbum\-title\=\"(.+)\"\salbum-link\=\"(.+)\" first-image\=\"(.+)\"\%\}$/,
+    // Function to extract data elements from the regexp match
+    fromBlock: function(match) {return {album_title: match[1], album_link: match[2], album_image: match[3]},
+    // Function to create a text block from an instance of this component
+    toBlock: function(obj) {return '{%include flickr-embed.html album-title="'+obj.album_title+'" album-link="'+obj.album_link+'" first-image="'+obj.album_image+'"%}';},
+    // Preview output for this component. Can either be a string or a React component
+    // (component gives better render performance)
+    toPreview: function(obj) {return ('<img src="'+ obj.album_image + '" alt="Youtube Video"/>');}
+});
+
